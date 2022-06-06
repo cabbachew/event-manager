6aad2c1774dc3e23af68cdf083b33761c62a726f
Daniel Oh <cabbachew@gmail.com>
2022-06-06 12:23:30 +0900

    Complete Assignment: Time Targeting

--- lib/time_targeting.rb
+++ lib/time_targeting.rb
@@ -1,4 +1,10 @@
 require 'csv'
+require 'time'
+
+def get_reg_hour(reg_date)
+  reg_hour = Time.strptime(reg_date, "%m/%d/%y %k:%M").hour # OR strftime("%l %p")
+end
+
 puts "Assignment: Time Targeting"
 
 contents = CSV.open(
@@ -7,9 +13,12 @@ contents = CSV.open(
   header_converters: :symbol
 )
 
-contents.each do |row|
+contents.each_with_object(Hash.new(0)) do |row, hash|
   name = row[:first_name]
-  reg_date = row[:regdate]
+  reg_hour = get_reg_hour(row[:regdate])
 
-  puts "#{name}: #{reg_date}"
+  puts "#{name}: #{reg_hour}"
+
+  hash[reg_hour] += 1
+  p hash.sort_by {|k,v| v}.reverse.to_h # In descending order
 end
